615f3dc9539046a935fabcc498f6c054cc089e74
Jailma santos <m.jailma07@gmail.com>
2019-02-21 09:42:50 -0300

    Exercicios

---
+++ lista3.py
@@ -0,0 +1,29 @@
+def main():
+     teste1 = [] # lista vazia
+    print("Soma de", teste1, "= ", soma_lista(teste1))
+
+    teste2 = [2] #
+    print("Soma de", teste2, "= ", soma_lista(teste2))
+
+    teste3 = [1,2,3]
+    print("Soma de", teste3, "= ", soma_lista(teste3))
+
+    teste4 = [0, -1, 7, 2, 5]
+    print("Soma de", teste4, "= ", soma_lista(teste4))
+''' teste da funcao maximo '''
+# CRIE OS SEUS TESTES
+#
+
+
+def max_lista(lista):
+    for i in lista:
+        
+''' (list) -> int, int
+    recebe uma lista de inteiros e retorna o
+    valor maximo da lista e o indice onde ele ocorre '''
+
+    # ESCREVA A SUA FUNCAO
+
+    return 0, 0
+
+main()
